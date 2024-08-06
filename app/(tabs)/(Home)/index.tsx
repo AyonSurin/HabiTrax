b0ac78a68afd0627225dc4ad695201c27d55a617
Jacob Sebastian Cyril <jacobsebcyril@gmail.com>
2024-08-06 11:41:25 +0530

    made minor changes to navbar

--- app/(tabs)/(Home)/index.tsx
+++ app/(tabs)/(Home)/index.tsx
@@ -13,6 +13,7 @@ export default function HomeScreen() {
       <Habit />
       <Habit />
       <Habit />
+      <Habit />
     </View>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,22 @@
-import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 
 export default function NavBar() {
   return (
     <View style={styles.container}>
       <Image
-        style={styles.navbar_logo}
+        style={styles.logo}
         source={require("../assets/images/navbar_logo.png")}
       />
-      <Image
-        style={styles.addIcon}
-        source={require("../assets/images/add_habit.png")}
-      />
-      <Image
-        style={styles.profileIcon}
-        source={require("../assets/images/profile.png")}
-      />
+      <View style={styles.iconsContainer}>
+        <Image
+          style={styles.icon}
+          source={require("../assets/images/add_habit.png")}
+        />
+        <Image
+          style={styles.icon}
+          source={require("../assets/images/profile.png")}
+        />
+      </View>
     </View>
   );
 }
@@ -22,23 +24,20 @@ export default function NavBar() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
-    // backgroundColor: "#ffb8b3",
+    justifyContent: "space-between",
+    alignItems: "center",
     width: "100%",
-    top: 5,
+    padding: 10,
   },
-  navbar_logo: {
-    position: "relative",
-    alignSelf: "flex-start",
-    backgroundColor: "b3fcff",
-    marginLeft: 30,
-    marginBottom: 15,
-    marginRight: 235,
+  logo: {
+    marginLeft: 10,
   },
-  addIcon: {
-    top: 7,
-    marginRight: 40,
+  iconsContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginRight: 10,
   },
-  profileIcon: {
-    top: 7,
+  icon: {
+    marginHorizontal: 15,
   },
 });
